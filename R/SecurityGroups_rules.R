85b020b85a8c9cc8f62fb192f05f8ae05e6743e5
Thomas J. Leeper <thosjleeper@gmail.com>
2018-07-24 10:32:37 +0100

    exposure revoke_ingress() and revoke_egress()

--- R/SecurityGroups_rules.R
+++ R/SecurityGroups_rules.R
@@ -1,6 +1,6 @@
 #' @rdname sg_ingress
-#' @title Security Group Ingress
-#' @description Authorize/Revoke Security Group Ingress
+#' @title Security Group Egress/Ingress
+#' @description Authorize/Revoke Security Group Egress/Ingress
 #' @template sgroup
 #' @param cidr A one- or two-element character vector specifying an IP or range of IP addresses. The default is your current machine's public IP as returned by <http://checkip.amazonaws.com/>.
 #' @param port A one- or two-element integer vector, specifying a port or port range.
@@ -12,27 +12,29 @@
 #' s <- describe_subnets()
 #' g <- create_sgroup("test_group", "new security group", vpc = s[[1]])
 #' authorize_ingress(g, port = 80, protocol = "tcp")
+#' authorize_egress(g, port = 80, protocol = "tcp")
 #' 
 #' # cleanup
 #' revoke_ingress(g, port = 80, protocol = "tcp")
+#' revoke_egress(g, port = 80, protocol = "tcp")
 #' }
 #' @keywords security
 #' @seealso [create_sgroup()]
 #' @export
-authorize_ingress <- function(sgroup, cidr = paste0(my_ip(),"/32"), port, protocol = "-1", ...) {
+authorize_ingress <- function(sgroup, cidr = paste0(my_ip(),"/32"), port = NULL, protocol = "-1", ...) {
     query <- list(Action = "AuthorizeSecurityGroupIngress")
     query$GroupId <- get_sgid(sgroup)
-    if (!missing(cidr)) {
+    if (!is.null(cidr)) {
         query$IpPermissions.1.IpRanges.1.CidrIp <- head(cidr, 1)
         if (length(cidr) == 2) {
             query$IpPermissions.1.IpRanges.2.CidrIp <- tail(cidr, 1)        
         }
     }
-    if (!missing(port)) {
+    if (!is.null(port)) {
         query$IpPermissions.1.FromPort <- head(port, 1)
         query$IpPermissions.1.ToPort <- tail(port, 1)
     }
-    if (!missing(protocol)) {
+    if (!is.null(protocol)) {
         query$IpPermissions.1.IpProtocol <- protocol
     }
     r <- ec2HTTP(query = query, ...)
@@ -70,8 +72,56 @@ revoke_ingress <- function(sgroup, cidr = paste0(my_ip(),"/32"), port, protocol 
     }
 }
 
+#' @importFrom utils head tail
+#' @rdname sg_ingress
+#' @export
+authorize_egress <- function(sgroup, cidr = paste0(my_ip(),"/32"), port = NULL, protocol = "-1", ...) {
+    query <- list(Action = "AuthorizeSecurityGroupEgress")
+    query$GroupId <- get_sgid(sgroup)
+    if (!is.null(cidr)) {
+        query$IpPermissions.1.IpRanges.1.CidrIp <- head(cidr, 1)
+        if (length(cidr) == 2) {
+            query$IpPermissions.1.IpRanges.2.CidrIp <- tail(cidr, 1)        
+        }
+    }
+    if (!is.null(port)) {
+        query$IpPermissions.1.FromPort <- head(port, 1)
+        query$IpPermissions.1.ToPort <- tail(port, 1)
+    }
+    if (!is.null(protocol)) {
+        query$IpPermissions.1.IpProtocol <- protocol
+    }
+    r <- ec2HTTP(query = query, ...)
+    if (r$return[[1]] == "true") {
+        return(TRUE)
+    } else {
+        return(FALSE)
+    }
+}
 
-authorize_egress <- function() {}
-
-revoke_egress <- function() {}
-
+#' @importFrom utils head tail
+#' @rdname sg_ingress
+#' @export
+revoke_egress <- function(sgroup, cidr = paste0(my_ip(),"/32"), port, protocol = "-1", ...) {
+    query <- list(Action = "RevokeSecurityGroupEgress")
+    query$GroupId <- get_sgid(sgroup)
+    if (!missing(cidr)) {
+        query$IpPermissions.1.IpRanges.1.CidrIp <- head(cidr, 1)
+        if (length(cidr) == 2) {
+            query$IpPermissions.1.IpRanges.2.CidrIp <- tail(cidr, 1)        
+        }
+    }
+    if (!missing(port)) {
+        query$IpPermissions.1.FromPort <- head(port, 1)
+        query$IpPermissions.1.ToPort <- tail(port, 1)
+    }
+    if (!missing(protocol)) {
+        query$IpPermissions.1.IpProtocol <- protocol
+    }
+    r <- ec2HTTP(query = query, ...)
+    if (r$return[[1]] == "true") {
+        return(TRUE)
+    } else {
+        return(FALSE)
+    }
+}
