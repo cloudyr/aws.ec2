6371758bf786852aa30b8f1f12030552d230f1d2
Jon Harmon <jonthegeek@gmail.com>
2020-04-15 10:55:01 -0500

    Removed ssh from suggests I need to see what it's for, but first I want to get Travis to work.

---
+++ tests/testthat.R
@@ -0,0 +1,12 @@
+k1 <- Sys.getenv("AWS_ACCESS_KEY_ID")
+k2 <- Sys.getenv("AWS_SECRET_ACCESS_KEY")
+if ((k1 != "") && (k2 != "")) {
+  library(testthat)
+  library(aws.ec2)
+  test_check("aws.ec2")
+} else {
+  stop(
+    "You must configure environment variables to run these tests.",
+    "\nSee README.md for details."
+  )
+}
